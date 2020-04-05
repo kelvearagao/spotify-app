50a15058da051b371b09497299d79ca94f67d13a
Kelve Aragão <kelvearagao@gmail.com>
2020-04-05 20:41:49 -0300

    Testa rota gh pages

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,9 @@ const baseConfig = require('./webpack.config.base')
 
 module.exports = env => merge(
   baseConfig(env), {
+    output: {
+      publicPath: '/' + require("./package.json").homepage
+    },
     mode: 'production'
   }
 )
